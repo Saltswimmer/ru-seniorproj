32fec5a9caec182bf00cecd8309966c0f57b3093
Saltswimmer <ethanciavo@gmail.com>
2023-03-09 22:36:04 -0500

    Added textfields and dummied login/signup buttons

--- harbour_frontend/lib/pages/credentials.dart
+++ harbour_frontend/lib/pages/credentials.dart
@@ -3,48 +3,168 @@
 import 'package:flutter/material.dart';
 import 'package:harbour_frontend/text_templates.dart';
 
-abstract class CredentialsPage extends StatelessWidget {
-  const CredentialsPage({Key? key}) : super(key: key);
+class CredentialsPageMixin {
 
-  @override
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+  late final TextEditingController _confirmPassword;
+
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
       padding: const EdgeInsets.all(8.0),
       alignment: Alignment.topCenter,
       child: FittedBox(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: body(context),
+        child: ConstrainedBox(
+          constraints: BoxConstraints(maxWidth: 500.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: body(context),
+          ),
         ),
       ),
     ));
   }
 
-  // The login and register pages will override this with their own widgets
   List<Widget> body(BuildContext context) {
     return [];
   }
 }
 
-class LoginPage extends CredentialsPage {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+class _LoginPageState extends State<LoginPage> with CredentialsPageMixin {
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    _confirmPassword = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    _confirmPassword.dispose();
+    super.dispose();
+  }
 
   @override
   List<Widget> body(BuildContext context) {
     return [
-      TextTemplates.headline("Log in", Theme.of(context).colorScheme.onPrimary),
+      TextTemplates.headline('Log in', Theme.of(context).colorScheme.onPrimary),
+
+      // EMAIL
+      TextField(
+        autocorrect: false,
+        enableSuggestions: false,
+        controller: _email,
+        decoration: InputDecoration(
+          hintText: 'Enter your email address',
+        ),
+      ),
+
+      // PASSWORD
+      TextField(
+        autocorrect: false,
+        enableSuggestions: false,
+        obscureText: true,
+        controller: _password,
+        decoration: InputDecoration(
+          hintText: 'Enter your password',
+        ),
+      ),
+
+      // LOGIN BUTTON
+      TextButton(
+          onPressed: () async {
+            final email = _email.text;
+            final password = _password.text;
+
+            // Do some auth stuff here
+          },
+          child: const Text('Log in')),
     ];
   }
 }
 
-class RegisterPage extends CredentialsPage {
+class RegisterPage extends StatefulWidget {
   const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+class _RegisterPageState extends State<RegisterPage> with CredentialsPageMixin {
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    _confirmPassword = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    _confirmPassword.dispose();
+    super.dispose();
+  }
 
   @override
   List<Widget> body(BuildContext context) {
     return [
-      TextTemplates.headline("Sign up", Theme.of(context).colorScheme.onPrimary),
+      TextTemplates.headline(
+          'Sign up', Theme.of(context).colorScheme.onPrimary),
+
+      // EMAIL
+      TextField(
+        autocorrect: false,
+        enableSuggestions: false,
+        keyboardType: TextInputType.emailAddress,
+        controller: _email,
+        decoration: InputDecoration(
+          hintText: 'Enter your email address',
+        ),
+      ),
+
+      // PASSWORD
+      TextField(
+        autocorrect: false,
+        enableSuggestions: false,
+        obscureText: true,
+        controller: _password,
+        decoration: InputDecoration(
+          hintText: 'Enter a strong password',
+        ),
+      ),
+
+      // CONFIRM PASSWORD
+      TextField(
+        autocorrect: false,
+        enableSuggestions: false,
+        obscureText: true,
+        controller: _confirmPassword,
+        decoration: InputDecoration(
+          hintText: 'Confirm your password',
+        ),
+      ),
+
+      // REGISTER BUTTON
+      TextButton(
+          onPressed: () async {
+            final email = _email.text;
+            final password = _password.text;
+            final confirmPassword = _confirmPassword.text;
+          },
+          child: const Text('Register')),
     ];
   }
 }
