f57e7d71227621ba71f68ca706ef8cc8360ac09a
Shane Cleary <shanedavidcleary@gmail.com>
2023-03-12 16:40:53 -0400

    added getUser endpoint, and tests

--- backend/pkg/common/router/user.go
+++ backend/pkg/common/router/user.go
@@ -2,14 +2,15 @@ package router
 
 import (
 	// "github.com/google/uuid"
-	"github.com/golang-jwt/jwt/v4"
-	"github.com/google/uuid"
-	"github.com/labstack/echo/v4"
+	"database/sql"
 	"net/http"
 	"time"
+
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
 )
 
-var sampleSecretKey = []byte("secret")
+// var sampleSecretKey = []byte("secret")
 
 type signupReq struct {
 	First_name  string `json:"first_name"`
@@ -17,6 +18,15 @@ type signupReq struct {
 	Last_name   string `json:"last_name"`
 	Username    string `json:"username"`
 	Email       string `json:"email"`
+}
+
+type User struct {
+	First_name  string `json:"first_name"`
+	Middle_name string `json:"middle_name"`
+	Last_name   string `json:"last_name"`
+	Username    string `json:"username"`
+	Email       string `json:"email"`
+	User_Id     string `json:"user_id"`
 }
 
 func (h *Handler) SignUp(c echo.Context) error {
@@ -28,25 +38,44 @@ func (h *Handler) SignUp(c echo.Context) error {
 	}
 
 	//define the sql statement to use for this endpoint
-	sql := `INSERT INTO users (first_name, middle_name, last_name, username, email, date_created) VALUES ($1, $2, $3, $4, $5, $6, $7)`
-	date := time.Now().Format("2006-01-02")
+	sql := `INSERT INTO users (first_name, middle_name, last_name, username, email, date_created, user_id) VALUES ($1, $2, $3, $4, $5, $6, $7)`
+	date := time.Now()
 	//fmt.Println(req.First_name, req.Middle_name, req.Last_name, req.Username, req.Email, date)
 	id := uuid.New()
 	_, err = h.db.Exec(sql, req.First_name, req.Middle_name, req.Last_name, req.Username, req.Email, date, id.String())
 	if err != nil {
+		panic(err)
 	}
 
-	token := jwt.New(jwt.SigningMethodHS256)
-	claims := token.Claims.(jwt.MapClaims)
-	//expire in ten minutes
-	claims["exp"] = time.Now().Add(10 * time.Minute).Unix()
-	claims["authorized"] = true
-	claims["user"] = req.Username
+	// token := jwt.New(jwt.SigningMethodHS256)
+	// claims := token.Claims.(jwt.MapClaims)
+	// //expire in ten minutes
+	// claims["exp"] = time.Now().Add(10 * time.Minute).Unix()
+	// claims["authorized"] = true
+	// claims["user"] = req.Username
 
-	tokenString, err := token.SignedString(sampleSecretKey)
-	if err != nil {
-		return err
+	// tokenString, err := token.SignedString(sampleSecretKey)
+	// if err != nil {
+	// 	return err
+	// }
+
+	return c.String(http.StatusOK, id.String())
+}
+
+func (h *Handler) GetUser(c echo.Context) error {
+
+	id := c.Param("id")
+
+	//define the sql statement to use for this endpoint
+	q := `SELECT first_name, middle_name, last_name, username, email, user_id FROM users WHERE user_id = ?`
+	row := h.db.QueryRow(q, id)
+
+	var u User
+	if err := row.Scan(&u.First_name, &u.Middle_name, &u.Last_name, &u.Username, &u.Email, &u.User_Id); err != nil {
+		if err == sql.ErrNoRows {
+			return c.String(http.StatusNotFound, "")
+		}
+		return c.JSONPretty(http.StatusOK, u, "\t")
 	}
-
-	return c.String(http.StatusOK, tokenString)
+	return c.String(http.StatusInternalServerError, "")
 }
--- backend/pkg/common/router/user_test.go
+++ backend/pkg/common/router/user_test.go
@@ -3,12 +3,12 @@ package router
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	"github.com/golang-jwt/jwt/v4"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 )
@@ -41,13 +41,32 @@ func TestSignup(t *testing.T) {
 	b, err := io.ReadAll(rec.Body)
 	assert.NoError(t, err)
 
-	tknStr := string(b)
-	//tkn := &jwt.MapClaims{}
-	tkn, err := jwt.Parse(tknStr, func(token *jwt.Token) (interface{}, error) {
-		return sampleSecretKey, nil
-	})
-	//claims, _ := tkn.Claims.(jwt.MapClaims)
+	// TEMP: returning user id
+	id := string(b)
+	req, rec = makeRequest(http.MethodGet, "/", nil)
+	c = testEcho.NewContext(req, rec)
+	c.SetPath("/users/:id")
+	c.SetParamNames("id")
+	c.SetParamValues(id)
+
+	if assert.NoError(t, testHandler.GetUser(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+	}
+
+	b, err = io.ReadAll(rec.Body)
 	assert.NoError(t, err)
-	assert.NotNil(t, tkn)
-	//assert.Equal(t, claims["user"], "jimbob")
+
+	// TEMP: returning user id
+	ujson := string(b)
+	fmt.Println(ujson)
+
+	// tknStr := string(b)
+	// //tkn := &jwt.MapClaims{}
+	// tkn, err := jwt.Parse(tknStr, func(token *jwt.Token) (interface{}, error) {
+	// 	return sampleSecretKey, nil
+	// })
+	// //claims, _ := tkn.Claims.(jwt.MapClaims)
+	// assert.NoError(t, err)
+	// assert.NotNil(t, tkn)
+	// //assert.Equal(t, claims["user"], "jimbob")
 }
