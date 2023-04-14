9d68c686ae6ec1a1d2ee084a18a801145d34b9e2
Heits <heits.software@gmail.com>
2023-04-14 19:35:40 -0400

    userlist frontend

--- harbour_frontend/lib/pages/vessel/users-vessels.dart
+++ harbour_frontend/lib/pages/vessel/users-vessels.dart
@@ -1,53 +1,34 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:harbour_frontend/models/user_model.dart';
 
 //THIS FILE IS CURRENTLY STANDALONE AND WILL HAVE TO BE INTEGRATED
 //TO INCLUDE THE CORRECT DATA STRUCTURES AND JSON REQUESTS FROM
 //VESSELS.dart FILE
 
-class User {
-  String name;
-  String email;
-  String profileImage;
-
-  User(this.name, this.email, this.profileImage);
-}
-
-class UserList extends StatelessWidget {
+class UserList extends StatefulWidget {
   final List<User> users;
 
   UserList({required this.users});
 
   @override
+  _UserListState createState() => _UserListState();
+}
+
+class _UserListState extends State<UserList> {
+
+  @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: users.length,
+      itemCount: widget.users.length,
       itemBuilder: (BuildContext context, int index) {
-        User user = users[index];
+        User user = widget.users[index];
         return ListTile(
-          leading: CircleAvatar(backgroundImage: NetworkImage(user.profileImage)),
-          title: Text(user.name),
+          title: Text(user.username),
           subtitle: Text(user.email),
         );
       },
-    );
-  }
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    List<User> exampleUsers = [
-      User("John Doe", "johndoe@example.com", "https://example.com/profile_image1.jpg"),
-      User("Jane Smith", "janesmith@example.com", "https://example.com/profile_image2.jpg"),
-      User("Bob Johnson", "bobjohnson@example.com", "https://example.com/profile_image3.jpg"),      // Add more example users here...
-    ];
-
-    return MaterialApp(
-      title: 'My App',
-      home: Scaffold(
-        appBar: AppBar(title: Text('User List')),
-        body: UserList(users: exampleUsers),
-      ),
     );
   }
 }
--- harbour_frontend/lib/pages/vessel/vessel.dart
+++ harbour_frontend/lib/pages/vessel/vessel.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 import 'chatpanel.dart';
+import 'users-vessels.dart';
 
 class VesselPage extends StatelessWidget {
   const VesselPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SafeArea(child: Row(
-        // columns go here
-        children: <Widget>[
+    return Scaffold(
+        body: SafeArea(
+            child: Row(
+                // columns go here
+                children: <Widget>[
           Column(),
           Expanded(child: ChatPanel()),
-          Column()])));
+          Column(
+              //child: UserList()
+                  ) // <-- This needs the argument of having the user list of the current vessel passed in
+        ])));
   }
 }
