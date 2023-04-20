ed61b488a4927c94f220813d7a2dad04a7a2c5f9
Heits <heits.software@gmail.com>
2023-04-20 13:50:57 -0400

    updates session.dart to include vessel token

--- harbour_frontend/lib/models/session.dart
+++ harbour_frontend/lib/models/session.dart
@@ -1,5 +1,7 @@
 import 'package:harbour_frontend/api/user_service.dart';
+import 'package:harbour_frontend/models/vessel_model.dart';
 
+import '../api/vessel_service.dart';
 import 'token.dart';
 import 'user_model.dart';
 
@@ -7,6 +9,7 @@ class Session {
   static bool _upToDate = false;
   static Token? _sessionToken;
   static User? _sessionUser;
+  static Vessel? _sessionVessel;
 
   static set upToDate(bool val) {
     _upToDate = val;
@@ -20,6 +23,10 @@ class Session {
     return _upToDate ? _sessionUser : null;
   }
 
+  static Vessel? get vessel {
+    return _upToDate ? _sessionVessel : null;
+  }
+
   static set token(Token? token) {
     _sessionToken = token;
   }
@@ -28,10 +35,16 @@ class Session {
     _sessionUser = user;
   }
 
+  static set vessel(Vessel? vessel) {
+    _sessionVessel = vessel;
+  }
+
   static Future<bool> refresh() async {
     late User user;
     try {
       user = await UserService().getUser(_sessionToken!);
+      //vessel = await UserService().getVessels(_sessionToken!);
+      //This is where function is going to get called when vessel is selected
     } on Exception catch (e) {
       upToDate = false;
       return false;
